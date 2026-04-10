3a6ecde178788e11cc0238fe4bb215387c8c92da
Phel-Viwath <phelviwathdj009@gmail.com>
2026-04-10 22:55:21 +0700

    addddd

--- practice_module_app/src/main/java/com/viwath/practice_module_app/drag_drop2/screen1.kt
+++ practice_module_app/src/main/java/com/viwath/practice_module_app/drag_drop2/screen1.kt
@@ -16,12 +16,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
